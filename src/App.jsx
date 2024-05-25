c8976bdf088da2ac46038ddcaceeef6066e4a89c
RishabhIndia <rishabh4a@gmail.com>
2024-05-25 14:00:43 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <h1 className="center">Learn Web Development</h1>
+
+      <div>
+        <p>
+          React has been designed from the start for gradual adoption, and you
+          can use as little or as much React as you need. Whether you want to
+          get a taste of React, add some interactivity to a simple HTML page, or
+          start a complex React-powered app, the links in this section will help
+          you get started.
+        </p>
+      </div>
+
+      <div>
+        <p>
+          As your application grows, you might want to consider a more
+          integrated setup. There are several JavaScript toolchains we recommend
+          for larger applications. Each of them can work with little to no
+          configuration and lets you take full advantage of the rich React
+          ecosystem. Learn how.
+        </p>
+      </div>
+
+      <div>
+        <p>
+          If you feel that the React documentation goes at a faster pace than
+          you’re comfortable with, check out this overview of React by Tania
+          Rascia. It introduces the most important React concepts in a detailed,
+          beginner-friendly way. Once you’re done, give the documentation
+          another try!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App
